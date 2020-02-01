21690ee6bc45484e182910a741f15758cfcbb511
Renaud Dumont <renaud.dumont@live.fr>
2020-02-01 17:57:45 +0100

    added lesson 11

---
+++ lesson-10/firstapp/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Ma première app React</h1>
+      </header>
+      <div>Test</div>
+    </div>
+  );
+}
+
+export default App;
